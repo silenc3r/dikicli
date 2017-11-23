1164e537a7990c832cd61883bc0dc0d8d799263f
Dawid Zych <dawid.zych@yandex.com>
2017-11-23 19:07:14 +0100

    Ignore pol-ang translations

--- dikicli/core.py
+++ dikicli/core.py
@@ -7,21 +7,19 @@ URL = 'https://www.diki.pl/%s'
 
 def parse(html_dump):
     soup = BeautifulSoup(html_dump, 'html.parser')
-    results = soup.find('div', class_='diki-results-left-column')
-
-    # if translation wasn't found check if there are any suggestions
-    if not results:
+    for r in soup.find_all('div', class_='diki-results-left-column'):
+        meanings_div = r.find_all('ol', class_='foreignToNativeMeanings')
+        if meanings_div:
+            parts = [p.get_text().strip()
+                     for p in r.select('div.partOfSpeechSectionHeader')]
+            assert len(parts) == len(meanings_div)
+            break
+    else:
+        # if translation wasn't found check if there are any suggestions
         suggestions = soup.find('div', class_='dictionarySuggestions')
         if suggestions:
             return suggestions.get_text().strip()
         return "Nie znaleziono tłumaczenia wpisanej frazy"
-
-    # parts of the speech are in separate divs from translations
-    parts = [p.get_text().strip()
-             for p in results.select('div.partOfSpeechSectionHeader')]
-
-    meanings_div = results.find_all('ol', class_='foreignToNativeMeanings')
-    assert len(parts) == len(meanings_div)
 
     translations = []
     for p, m in zip(parts, meanings_div):
