4af2589bceff07b34430b07a2e4a54f64b34c04e
Dawid Zych <dawid.zych@yandex.com>
2017-11-24 17:43:15 +0100

    Add support for writing html files

--- dikicli/cli.py
+++ dikicli/cli.py
@@ -3,42 +3,41 @@ import sys
 from pathlib import Path
 
 from .core import URL, HEADERS
-from .core import parse, write_to_file, check_file_exists
+from .core import WordNotFound
+from .core import parse, write_to_file, write_html_file, write_index_file
 
 
 def create_file_prompt(filename):
     print(f"File doesn't exist: {filename}")
     answer = input("Do you want to create it? (y/N) ").lower()
     if answer not in ['y', 'yes']:
-        print("aborting...")
+        print("aborting...", file=sys.stderr)
         sys.exit(1)
     else:
         parent = filename.parent
         if not parent.exists():
-            ap = input("Parent directory doesn't exist, "
-                       "do you want to create it? (y/N) ").lower()
-            if ap in ['y', 'yes']:
+            answer = input("Parent directory doesn't exist, "
+                           "do you want to create it? (y/N) ").lower()
+            if answer in ['y', 'yes']:
                 parent.mkdir(parents=True)
             else:
-                print("aborting...")
+                print("aborting...", file=sys.stderr)
                 sys.exit(1)
         filename.touch()
         print(f"New file crated: {filename}")
 
 
 def print_result(result):
+    # result is always a list
     # add line wrapping
-    if isinstance(result, list):
-        for t in result:
-            print("[{}]".format(t['part']))
-            for m in t['meanings_list']:
-                print("# ", end='')
-                print(', '.join(m['meaning']))
-                for e in m['examples']:
-                    print(e)
-            print()
-    else:
-        print(result)
+    for t in result:
+        print("[{}]".format(t['part']))
+        for m in t['meanings_list']:
+            print("# ", end='')
+            print(', '.join(m['meaning']))
+            for e in m['examples']:
+                print(e)
+        print()
 
 
 def main():
@@ -47,15 +46,21 @@ def main():
     if len(sys.argv) != 2:
         return "Wrong arguments"
     word = sys.argv[1]
-    with requests.get(URL % word, headers=HEADERS) as r:
-        result = parse(r.content)
-
-    print_result(result)
+    with requests.get(URL.format(word=word), headers=HEADERS) as r:
+        try:
+            result = parse(r.content)
+        except WordNotFound as e:
+            print(str(e), file=sys.stderr)
+            sys.exit(2)
 
     wfile = Path('/tmp/phony.txt')
     prefix = '-'
 
-    if isinstance(result, list):
-        if not check_file_exists(wfile):
-            create_file_prompt(wfile)
-        write_to_file(wfile, word, prefix)
+    print_result(result)
+
+    if not wfile.is_file():
+        create_file_prompt(wfile)
+    write_to_file(wfile, word, prefix)
+
+    write_html_file(word, result)
+    write_index_file(wfile, prefix)
--- dikicli/core.py
+++ dikicli/core.py
@@ -2,9 +2,13 @@ import re
 from bs4 import BeautifulSoup
 from pathlib import Path
 
-URL = 'https://www.diki.pl/%s'
+URL = 'https://www.diki.pl/{word}'
 HEADERS = {'User-Agent': ('Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; '
                           'Trident/7.0;  rv:11.0) like Gecko')}
+
+
+class WordNotFound(Exception):
+    pass
 
 
 def parse(html_dump):
@@ -20,8 +24,8 @@ def parse(html_dump):
         # if translation wasn't found check if there are any suggestions
         suggestions = soup.find('div', class_='dictionarySuggestions')
         if suggestions:
-            return suggestions.get_text().strip()
-        return "Nie znaleziono tłumaczenia wpisanej frazy"
+            raise WordNotFound(suggestions.get_text().strip())
+        raise WordNotFound("Nie znaleziono tłumaczenia wpisanej frazy")
 
     translations = []
     for p, m in zip(parts, meanings_div):
@@ -42,9 +46,20 @@ def parse(html_dump):
     return translations
 
 
+def parse_cached(filename):
+    pass
+
+
+def cache_lookup(word, cache_dir):
+    # FIXME: location from config
+    filename = Path(cache_dir + '{}.html'.format(word))
+    if filename.is_file():
+        return parse_cached(filename)
+
+
 def get_words(words_file, prefix):
     with open(words_file, mode='rt') as f:
-        return set(l.rstrip()[len(prefix):] for l in f)
+        return [l.rstrip()[len(prefix):] for l in f]
 
 
 def write_to_file(words_file, word, prefix='-'):
@@ -53,6 +68,43 @@ def write_to_file(words_file, word, prefix='-'):
             f.write(prefix + word + '\n')
 
 
-def check_file_exists(filename):
-    filepath = Path(filename)
-    return filepath.is_file()
+def write_html_file(word, translations):
+    content = []
+    for t in translations:
+        content.append("<br>")
+        content.append("<p>[{part}]</p>".format(part=t['part']))
+        for i, m in enumerate(t['meanings_list'], 1):
+            content.append("<p><strong>{i}. {meaning}</p></strong>".format(
+                i=i, meaning=', '.join(m['meaning'])
+            ))
+            for e in m['examples']:
+                content.append("<p>{example}</p>".format(example=e))
+    content_str = '\n'.join(content)
+    # FIXME: should get location from config
+    with open('/tmp/template.html', mode='rt') as f:
+        result = f.read()
+        result = result.replace('{% word %}', word)
+        result = result.replace('{% content %}', content_str)
+    # FIXME: should get location from config
+    fname = Path('/tmp/translations/{}.html'.format(word))
+    with open(fname, mode='wt') as f:
+        f.write(result)
+
+
+def write_index_file(words_file, prefix):
+    content = ['<br>', '<ul>']
+    for word in get_words(words_file, prefix):
+        # FIXME: should get location from config
+        if Path('/tmp/translations/{word}.html'.format(word=word)).is_file():
+            content.append(('<li><a href="translations/{word}.html">'
+                            '{word}</a></li>').format(word=word))
+    content.append('</ul>')
+    content_str = '\n'.join(content)
+    # FIXME: should get location from config
+    with open('/tmp/template.html', mode='rt') as f:
+        result = f.read()
+        result = result.replace('{% word %}', "Index")
+        result = result.replace('{% content %}', content_str)
+    # FIXME: should get location from config
+    with open('/tmp/index.html', mode='wt') as f:
+        f.write(result)
