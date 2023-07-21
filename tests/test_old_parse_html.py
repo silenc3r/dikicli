13f17743d1e33e261c7e10cd7ceee4b58f2ef4bd
Dawid Zych <dwd@mailo.com>
2023-07-21 18:41:20 +0200

    Fix old parse tests

--- tests/test_old_parse_html.py
+++ tests/test_old_parse_html.py
@@ -54,43 +54,43 @@ class TestFlatten:
     def test_apple(self):
         html_dump = _lookup_online("apple")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
 
     def test_weight(self):
         html_dump = _lookup_online("weight")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
 
     def test_abandon(self):
         html_dump = _lookup_online("abandon")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
 
     def test_pet(self):
         html_dump = _lookup_online("pet")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
 
     def test_snitch(self):
         html_dump = _lookup_online("snitch")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
 
     def test_guest(self):
         html_dump = _lookup_online("guest")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
 
     def test_sad(self):
         html_dump = _lookup_online("sad")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
 
     def test_alternative_wordings(self):
@@ -107,23 +107,23 @@ class TestFlatten:
         ]:
             html_dump = _lookup_online(word)
             result = parse_en_pl(html_dump)
-            f_result = flatten(_parse_html(html_dump))
+            f_result = flatten(_parse_html(html_dump.html))
             assert result == f_result
 
     def test_would(self):
         html_dump = _lookup_online("would")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
 
     def test_tumult(self):
         html_dump = _lookup_online("tumult")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
 
     def test_subordinate(self):
         html_dump = _lookup_online("subordinate")
         result = parse_en_pl(html_dump)
-        f_result = flatten(_parse_html(html_dump))
+        f_result = flatten(_parse_html(html_dump.html))
         assert result == f_result
