99f53f3e09a016ce29b5b577e3f84c70e2674fb2
Dawid Zych <dwd@mailo.com>
2025-02-09 18:45:06 +0100

    cleanup

--- tests/generate1.py
+++ tests/generate1.py
@@ -2,17 +2,18 @@ import pathlib
 
 DIR = pathlib.Path(__file__).resolve().parent
 
+
 def generate():
     with open(DIR / "test_compare_new.py", mode="w") as f:
-        f.write("""\
+        f.write(
+            """\
 import os
 import tempfile
 import shutil
 import pytest
 from dikicli.core import lookup_online
-from dikicli.core import _parse_html, parse_en_pl
+from dikicli.core import parse_en_pl
 from dikicli.core import WordNotFound
-from dikicli.helpers import to_dict
 
 
 @pytest.fixture(autouse=True)
@@ -34,85 +35,123 @@ def env():
     os.environ.update(orig_env)
 
 
-""")
+"""
+        )
 
-        f.write("""\
+        f.write(
+            """\
 @pytest.mark.vcr
 class TestCompareComma:
-""")
-        for w in ["hubris", "misfit", "wiry", "until", "scoop", "strain", "work", "hog", "gangster", "AC", "hotel", "pet", "problem"]:
+"""
+        )
+        for w in [
+            "hubris",
+            "misfit",
+            "wiry",
+            "until",
+            "scoop",
+            "strain",
+            "work",
+            "hog",
+            "gangster",
+            "AC",
+            "hotel",
+            "pet",
+            "problem",
+        ]:
             safe_w = w.replace("'", "_").replace("-", "_").replace(" ", "_")
-            f.write(f"""\
+            f.write(
+                f"""\
     def test_compare_{safe_w}(self):
         html_dump = lookup_online("{w}")
         result = parse_en_pl(html_dump)
         f_result = to_dict(_parse_html(html_dump))
         assert result == f_result
 
-""")
-        f.write("""\
+"""
+            )
+        f.write(
+            """\
 @pytest.mark.vcr
 class TestCompareNoPoS:
-""")
+"""
+        )
         for w in ["guest", "kind of", "sort of"]:
             safe_w = w.replace("'", "_").replace("-", "_").replace(" ", "_")
-            f.write(f"""\
+            f.write(
+                f"""\
     def test_compare_{safe_w}(self):
         html_dump = lookup_online("{w}")
         result = parse_en_pl(html_dump)
         f_result = to_dict(_parse_html(html_dump))
         assert result == f_result
 
-""")
-        f.write("""\
+"""
+            )
+        f.write(
+            """\
 @pytest.mark.vcr
 class TestCompareNoExamples:
-""")
+"""
+        )
         for w in ["potent", "pining", "encrypt"]:
             safe_w = w.replace("'", "_").replace("-", "_").replace(" ", "_")
-            f.write(f"""\
+            f.write(
+                f"""\
     def test_compare_{safe_w}(self):
         html_dump = lookup_online("{w}")
         result = parse_en_pl(html_dump)
         f_result = to_dict(_parse_html(html_dump))
         assert result == f_result
 
-""")
-        f.write("""\
+"""
+            )
+        f.write(
+            """\
 @pytest.mark.vcr
 class TestCompareVariations:
-""")
+"""
+        )
         for w in ["tameable", "gold", "gradient", "GOP", "glamorising", "nice"]:
             safe_w = w.replace("'", "_").replace("-", "_").replace(" ", "_")
-            f.write(f"""\
+            f.write(
+                f"""\
     def test_compare_{safe_w}(self):
         html_dump = lookup_online("{w}")
         result = parse_en_pl(html_dump)
         f_result = to_dict(_parse_html(html_dump))
         assert result == f_result
 
-""")
-        f.write("""\
+"""
+            )
+        f.write(
+            """\
 @pytest.mark.vcr
 class TestCompareOther:
-""")
+"""
+        )
         for w in ["shutdown", "injustice", "tint", "deferral"]:
             safe_w = w.replace("'", "_").replace("-", "_").replace(" ", "_")
-            f.write(f"""\
+            f.write(
+                f"""\
     def test_compare_{safe_w}(self):
         html_dump = lookup_online("{w}")
         result = parse_en_pl(html_dump)
         f_result = to_dict(_parse_html(html_dump))
         assert result == f_result
 
-""")
-        f.write("""\
+"""
+            )
+        f.write(
+            """\
 @pytest.mark.vcr
 class TestCompareNotFound:
-""")
+"""
+        )
         for w in ["zzzzzz", "greaves", "reconnoitered", "ayahyuasca"]:
             safe_w = w.replace("'", "_").replace("-", "_").replace(" ", "_")
-            f.write(f"""\
+            f.write(
+                f"""\
     def test_compare_{safe_w}(self):
         html_dump = lookup_online("{w}")
         try:
@@ -125,14 +164,18 @@ class TestCompareNotFound:
             f_result = str(exn)
         assert result == f_result
 
-""")
-        f.write("""\
+"""
+            )
+        f.write(
+            """\
 @pytest.mark.vcr
 class TestCompareProfanity:
-""")
+"""
+        )
         for w in ["yam"]:
             safe_w = w.replace("'", "_").replace("-", "_").replace(" ", "_")
-            f.write(f"""\
+            f.write(
+                f"""\
     def test_compare_{safe_w}(self):
         html_dump = lookup_online("{w}")
         try:
@@ -145,7 +188,9 @@ class TestCompareProfanity:
             f_result = str(exn)
         assert result == f_result
 
-""")
+"""
+            )
+
 
 if __name__ == "__main__":
     generate()
